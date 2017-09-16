2a5213640950fc2af1b461921571c597a3cbd32a
Sravan Kumar Kilaru <kilarusravankumar@gmail.com>
2017-09-15 20:27:08 -0400

    Compression added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express=require('express');
+var compression=require('compression');
 var app=express();
 const path=require('path');
 
+app.use(compression());
 app.use('/', express.static('public'));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
-var port=Number(process.env.PORT || 3000);
+var port=Number(3000);
 app.listen(port);
